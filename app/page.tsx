e6b2ab88d0def354680279f7fadd01ab082c97f9
semanda <patash248@gmail.com>
2024-07-10 17:12:46 +0200

    add success page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,35 @@
 import Image from "next/image";
+import Link from "next/link";
 import { PatientForm } from "@/components/forms/PatientForm";
 export default function Home() {
   return (
     <main className="min-h-screen  p-10">
       <h1 className="flex justify-center items-center lg:text-4xl sm:text-xl font-bold">MEDOC <span>+</span></h1>
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+ <PatientForm />
+         
 
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 Medoc
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
     
 
-      <PatientForm /> 
+   
           
     </main>
   );
