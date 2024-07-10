3daed82f31865130747922bb9d2224533d068959
semanda <patash248@gmail.com>
2024-07-10 16:43:08 +0200

    appointment page added

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 import { PatientForm } from "@/components/forms/PatientForm";
 export default function Home() {
   return (
-    <main className="flex min-h-screen  p-10">
-      
+    <main className="min-h-screen  p-10">
+      <h1 className="flex justify-center items-center lg:text-4xl sm:text-xl font-bold">MEDOC <span>+</span></h1>
 
     
 
